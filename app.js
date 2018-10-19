379a3621b6a34269069a36b4b24839f0643c3643
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2018-10-19 11:17:26 +0200

    Show project with filters

--- app.js
+++ app.js
@@ -1,39 +1,84 @@
-// Init classes
-var git = {
-    nom: '',
-    prenom: '',
-    url: '',
-    commit_list: []
-}
-
-var commit = {}
-
 // Init conf variables
-var TOKEN = null;
-
-// Load app config
-$.getJSON("app.json", function(json) {
-    TOKEN = json.TOKEN;
-});
+var TOKEN = '6881346ff446427f2b903c856de5add948896420';
+var githubUri = 'https://api.github.com';
 
 var app = new Vue({
     el: '#app',
     data: {
-        git_list: []
-    },
-    mounted() {
-
+        gitList: [],
+        authorList: [
+            'benjaminbra',
+            /*'Nair0fl',
+            'raphaelCharre',
+            'mathiasLoiret',
+            'thomaspich',
+            'TeofiloJ',
+            'Grigusky',
+            'Dakistos',
+            'mael61',
+            'KevinPautonnier',
+            'BenoitCochet',
+            'sfongue',
+            'ClementCaillaud',
+            'gfourny',
+            'Mokui',
+            'LordInateur',
+            'AntoineGOSSET',
+            'etienneYnov',
+            'Coblestone',
+            'AlexDesvallees',
+            'rudy8530',
+            'Killy85',
+            'alixnzt'*/
+        ],
+        projectList: [
+            'github-ynov-vue',
+        ],
+        projectSelected: [],
+        authorSelected: [],
     },
     methods: {
-        refreshProject: function () {
-            axios({
-                headers : {
-                    Accept: 'application/vnd.github.v3+json',
-                    Authorization: `token ${TOKEN}`
+        refreshProjects: function () {
+            for (let pS in this.projectSelected) {
+                for (let aS in this.authorSelected) {
+                    axios({
+                        headers: {
+                            Accept: 'application/vnd.github.v3+json',
+                            Authorization: `token ${TOKEN}`
+                        },
+                        method: 'GET',
+                        url: `${githubUri}/repos/${this.authorSelected[aS]}/${this.projectSelected[pS]}`
+                    })
+                        .then(function (response) {
+                            app.gitList = [];
+                            repo = response.data;
+
+                            var newGit = {};
+                            newGit.nom = repo.full_name;
+                            newGit.url = repo.html_url;
+                            newGit.commits = [];
+
+                            axios({
+                                headers: {
+                                    Accept: 'application/vnd.github.v3+json',
+                                    Authorization: `token ${TOKEN}`
+                                },
+                                method: 'GET',
+                                url: `${githubUri}/repos/${repo.full_name}/commits`
+                            })
+                                .then(function (response) {
+                                    for (c in response.data) {
+                                        var commit = {};
+                                        commit.message = response.data[c].commit.message;
+                                        newGit.commits.push(commit);
+                                    }
+                                    app.gitList.push(newGit);
+                                })
+
+                        })
                 }
-            })
-                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                .then(response => (this.info = response))
-        }
+            }
+        },
+
     }
 })
